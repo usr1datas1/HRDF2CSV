73cc45ff0d23084227ab37bf64fa5e8c179130ff
usr1datas1 <usr1@datas1.de>
2021-06-03 11:50:36 +0200

    Refactored

--- hrdf2csv.py
+++ hrdf2csv.py
@@ -1,6 +1,6 @@
 import re
 import sys
-
+import codecs
 
 class Station:
     def __init__(self, station_id, station_name):
@@ -12,7 +12,6 @@ class Station:
         self.coordinates = (None, None, None)
         self.platforms = set()
         self.mobihub_id = None
-
 
     def set_platforms(self, gleise):
         self.platforms = gleise
@@ -32,7 +31,7 @@ class Station:
     def add_metabhf(self, metabhf):
         self.metabhfs = metabhf
 
-    def get_metabhf(self, consider_none=True, max_transfer_time=sys.maxsize): #sys.maxsize
+    def get_metabhf(self, consider_none=True, max_transfer_time=sys.maxsize):  # sys.maxsize
         return list(filter(lambda x: consider_none if x[1] is None else int(x[1]) <= max_transfer_time, self.metabhfs))
 
     def get_metabhf_count(self):
@@ -61,14 +60,13 @@ class Station:
         return len(self.journeys)
 
     def get_mobihub_stop_count(self):
-       return self.mobihub_stop_count
+        return self.mobihub_stop_count
 
     def get_start_count(self):
         return len(list(filter(lambda x: x.has_start_stop(self.station_id), self.journeys)))
 
     def get_end_count(self):
         return len(list(filter(lambda x: x.has_end_stop(self.station_id), self.journeys)))
-
 
 
 class Stop:
@@ -118,10 +116,7 @@ class Journey:
         return self.stops[-1]
 
 
-hrdf_directory = 'data'
-
-
-def get_bahnhof():
+def get_bahnhof(hrdf_directory):
     result = {}
     with open('{}/BAHNHOF'.format(hrdf_directory), 'r', encoding='UTF-8') as f:
         lines = f.readlines()
@@ -132,10 +127,7 @@ def get_bahnhof():
     return result
 
 
-bahnhof = get_bahnhof()
-
-
-def get_metabhf():
+def get_metabhf(hrdf_directory):
     s = re.compile(r'^\*A\s')
     r = re.compile(r'^\d{7}\s')
     r2 = re.compile(r'^\d{7}\:\s\s(\d{7}.+)')
@@ -175,21 +167,7 @@ def get_metabhf():
     return result
 
 
-metabhf = get_metabhf()
-
-all_metabhf_ids = set()
-all_bahnhof_ids = set()
-
-for mbhf in metabhf.keys():
-    all_metabhf_ids.add(mbhf)
-
-for bhf in bahnhof.keys():
-    all_bahnhof_ids.add(bhf)
-
-exclude_set = all_metabhf_ids - all_bahnhof_ids
-
-
-def get_bfkoord():
+def get_bfkoord(hrdf_directory):
     result = {}
     with open('{}/BFKOORD_WGS'.format(hrdf_directory), 'r', encoding='UTF-8') as f:
         lines = f.readlines()
@@ -200,10 +178,7 @@ def get_bfkoord():
     return result
 
 
-bfkoord = get_bfkoord()
-
-
-def get_platforms():
+def get_platforms(hrdf_directory):
     rows = None
     with open('{}/GLEIS'.format(hrdf_directory), 'r', encoding='UTF-8') as f:
         rows = f.readlines()
@@ -227,10 +202,7 @@ def get_platforms():
     return result
 
 
-platforms = get_platforms()
-
-
-def get_zugart():
+def get_zugart(hrdf_directory):
     rows = None
     with open('{}/ZUGART'.format(hrdf_directory), 'r', encoding='UTF-8') as f:
         rows = f.readlines()
@@ -266,8 +238,6 @@ def get_zugart():
     return result
 
 
-zugart = get_zugart()
-
 vm_categories = {
     'Zug': ['AG', 'EC', 'EXT', 'IC', 'ICE', 'IR', 'IRE', 'MAT', 'NJ', 'NZ', 'PB', 'PE', 'R', 'RB', 'RE', 'RJX', 'S',
             'SN', 'TER', 'TGV', 'UUU'],
@@ -279,17 +249,15 @@ vm_categories = {
     'Metro': ['M'],
     'Aufzug': ['ASC']
 }
-[item for sublist in vm_categories.values() for item in sublist]
-pass
+
+
+# [item for sublist in vm_categories.values() for item in sublist]
 
 
 def get_vm_category(vehicle_type):
     for k, v in vm_categories.items():
         if vehicle_type in v:
             return k
-
-
-get_vm_category('BAT')
 
 
 def get_journeys():
@@ -333,53 +301,72 @@ def get_journeys():
     return result
 
 
-journeys = get_journeys()
+if __name__ == "__main__":
+    hrdf_directory = 'data'
 
-vehicle_types = set()
-for jrn in journeys:
-    vehicle_types.add(jrn.vehicle_type)
+    bahnhof = get_bahnhof(hrdf_directory)
+    zugart = get_zugart(hrdf_directory)
+    platforms = get_platforms(hrdf_directory)
+    bfkoord = get_bfkoord(hrdf_directory)
 
-print(vehicle_types)
+    metabhf = get_metabhf(hrdf_directory)
 
-sttns = {}
+    all_metabhf_ids = set()
+    all_bahnhof_ids = set()
 
-for station_id in bahnhof.keys():
-    s = Station(station_id, bahnhof[station_id])
+    for mbhf in metabhf.keys():
+        all_metabhf_ids.add(mbhf)
 
-    if station_id in bfkoord.keys():
-        s.add_coordinates(bfkoord[station_id])
+    for bhf in bahnhof.keys():
+        all_bahnhof_ids.add(bhf)
 
-    if station_id in platforms.keys():
-        s.set_platforms(platforms[station_id])
+    exclude_set = all_metabhf_ids - all_bahnhof_ids
 
-    if station_id in metabhf.keys():
-        s.add_metabhf(metabhf[station_id])
+    journeys = get_journeys()
 
-    sttns[station_id] = s
+    vehicle_types = set()
+    for jrn in journeys:
+        vehicle_types.add(jrn.vehicle_type)
 
-for journey in journeys:
-    for station_id in journey.stops:
-        sttns[station_id].add_journey(journey)
+    sttns = {}
 
-for sttn in sttns.values():
-    vehicle_categories_numbers = {}
-    for j in sttn.journeys:
-        vmctgr = get_vm_category(j.vehicle_type)
-        if vmctgr in vehicle_categories_numbers.keys():
-            vehicle_categories_numbers[vmctgr] += 1
-        else:
-            vehicle_categories_numbers[vmctgr] = 1
-    sttn.set_vehicle_categories_numbers(vehicle_categories_numbers)
+    for station_id in bahnhof.keys():
+        s = Station(station_id, bahnhof[station_id])
 
-    # mobihub_as_start_count = sttn.get_start_count()
-    # mobihub_as_end_count = sttn.get_end_count()
-    # for mbhf_id in sttn.get_metabhf():
-    #     if mbhf_id[0] not in exclude_set:  # EXCLUSION LIST !!!!
-    #         mbhf_sttn = sttns[mbhf_id[0]]
-    #         mobihub_as_start_count += mbhf_sttn.get_start_count()
-    #         mobihub_as_end_count += mbhf_sttn.get_end_count()
-    # sttn.set_mobihub_as_start_count(mobihub_as_start_count)
-    # sttn.set_mobihub_as_end_count(mobihub_as_end_count)
+        if station_id in bfkoord.keys():
+            s.add_coordinates(bfkoord[station_id])
+
+        if station_id in platforms.keys():
+            s.set_platforms(platforms[station_id])
+
+        if station_id in metabhf.keys():
+            s.add_metabhf(metabhf[station_id])
+
+        sttns[station_id] = s
+
+    for journey in journeys:
+        for station_id in journey.stops:
+            sttns[station_id].add_journey(journey)
+
+    for sttn in sttns.values():
+        vehicle_categories_numbers = {}
+        for j in sttn.journeys:
+            vmctgr = get_vm_category(j.vehicle_type)
+            if vmctgr in vehicle_categories_numbers.keys():
+                vehicle_categories_numbers[vmctgr] += 1
+            else:
+                vehicle_categories_numbers[vmctgr] = 1
+        sttn.set_vehicle_categories_numbers(vehicle_categories_numbers)
+
+        # mobihub_as_start_count = sttn.get_start_count()
+        # mobihub_as_end_count = sttn.get_end_count()
+        # for mbhf_id in sttn.get_metabhf():
+        #     if mbhf_id[0] not in exclude_set:  # EXCLUSION LIST !!!!
+        #         mbhf_sttn = sttns[mbhf_id[0]]
+        #         mobihub_as_start_count += mbhf_sttn.get_start_count()
+        #         mobihub_as_end_count += mbhf_sttn.get_end_count()
+        # sttn.set_mobihub_as_start_count(mobihub_as_start_count)
+        # sttn.set_mobihub_as_end_count(mobihub_as_end_count)
 
     mtbhfs = sttn.get_metabhf()
     if len(mtbhfs) > 0:
@@ -388,84 +375,83 @@ for sttn in sttns.values():
                 if sttns[mtb].mobihub_id is None:
                     sttns[mtb].mobihub_id = sttn.station_id
             except KeyError as e:
-                #print('\t', 'KEY ERROR:', mtb)
+                # print('\t', 'KEY ERROR:', mtb)
                 pass
 
         if sttn.mobihub_id is None:
             sttn.mobihub_id = sttn.station_id
 
+    for sttn in sttns.values():
+        mobihub_stop_count = sttn.get_stop_count()
+        for station_id, transfer_time in sttn.get_metabhf():
+            try:
+                mobihub_stop_count += sttns[station_id].get_stop_count()
+            except KeyError as e:
+                pass
+        sttn.mobihub_stop_count = mobihub_stop_count
 
-for sttn in sttns.values():
-    mobihub_stop_count = sttn.get_stop_count()
-    for station_id, transfer_time in sttn.get_metabhf():
-        try:
-            mobihub_stop_count += sttns[station_id].get_stop_count()
-        except KeyError as e:
-            pass
-    sttn.mobihub_stop_count = mobihub_stop_count
 
+    output_filename = 'csv_from_hrdf.csv'
 
-import codecs
-output_filename = 'data/csv_from_hrdf.csv'
+    with codecs.open(output_filename, 'w+b', encoding='UTF-8') as f:
+        f.write(u'\ufeff')
+        # vehicle_types_str = (';{}' * len(vehicle_types)).format(*vehicle_types)
+        vehicle_categories_str = (';{}' * len(vm_categories)).format(*vm_categories)
 
-with codecs.open(output_filename, 'w+b', encoding='UTF-8') as f:
-    f.write(u'\ufeff')
-    # vehicle_types_str = (';{}' * len(vehicle_types)).format(*vehicle_types)
-    vehicle_categories_str = (';{}' * len(vm_categories)).format(*vm_categories)
-
-    station_str = '{};{}'.format('station_id', 'station_name')
-    platforms_count_str = ';{}'.format('platforms_count')
-    metabhf_count_str = ';{}'.format('metabhf_count')
-    metabhf_str = ';{}'.format('metabhf_relations')
-    coords_str = ';{};{}'.format('lat', 'long')
-    start_end_count_str = ';{};{}'.format('as_start_count', 'as_end_count')
-    mobihub_str = ';{}'.format('mobihub_id')
-
-    f.write(
-        station_str +
-        mobihub_str +
-        # vehicle_types_str +
-        vehicle_categories_str +
-        metabhf_count_str +
-        metabhf_str +
-        platforms_count_str +
-        start_end_count_str +
-        coords_str + '\n')
-
-with codecs.open(output_filename, 'a+b', encoding='UTF-8') as f:
-    for station_id in sttns.keys():
-        station_str = '{};{}'.format(station_id, sttns[station_id].station_name)
-
-        ctgrs = [0] * len(vm_categories)
-        vtnrs = sttns[station_id].vehicle_categories
-        for vctgr in vtnrs:
-            index = list(vm_categories.keys()).index(vctgr)
-            ctgrs[index] += vtnrs[vctgr]
-        vehicle_categories_str = (';{}' * len(vm_categories)).format(*ctgrs)
-
-        metabhf_count_str = ';{}'.format(len(sttns[station_id].get_metabhf()))
-
-        metabhf_str = ';{}'.format(' '.join(
-            ['-'.join([str(mtb[0]), '-' if mtb[1] is None else '(' + str(int(mtb[1]))]) + 'min)' for mtb in
-             sttns[station_id].get_metabhf()]))
-
-        start_end_count_str = ';{};{}'.format(sttns[station_id].get_start_count(), sttns[station_id].get_end_count())
-
-        coords_str = ';{};{}'.format(sttns[station_id].get_lat(), sttns[station_id].get_long())
-
-        if station_id in platforms.keys():
-            platforms_count_str = ';{}'.format(sttns[station_id].get_platforms_count())
-        else:
-            platforms_count_str = ';{}'.format(0)
-
-        mobihub_str = ';{}'.format(sttns[station_id].mobihub_id)
+        station_str = '{};{}'.format('station_id', 'station_name')
+        platforms_count_str = ';{}'.format('platforms_count')
+        metabhf_count_str = ';{}'.format('metabhf_count')
+        metabhf_str = ';{}'.format('metabhf_relations')
+        coords_str = ';{};{}'.format('lat', 'long')
+        start_end_count_str = ';{};{}'.format('as_start_count', 'as_end_count')
+        mobihub_str = ';{}'.format('mobihub_id')
 
         f.write(
             station_str +
             mobihub_str +
+            # vehicle_types_str +
             vehicle_categories_str +
             metabhf_count_str +
             metabhf_str +
             platforms_count_str +
             start_end_count_str +
             coords_str + '\n')
+
+    with codecs.open(output_filename, 'a+b', encoding='UTF-8') as f:
+        for station_id in sttns.keys():
+            station_str = '{};{}'.format(station_id, sttns[station_id].station_name)
+
+            ctgrs = [0] * len(vm_categories)
+            vtnrs = sttns[station_id].vehicle_categories
+            for vctgr in vtnrs:
+                index = list(vm_categories.keys()).index(vctgr)
+                ctgrs[index] += vtnrs[vctgr]
+            vehicle_categories_str = (';{}' * len(vm_categories)).format(*ctgrs)
+
+            metabhf_count_str = ';{}'.format(len(sttns[station_id].get_metabhf()))
+
+            metabhf_str = ';{}'.format(' '.join(
+                ['-'.join([str(mtb[0]), '-' if mtb[1] is None else '(' + str(int(mtb[1]))]) + 'min)' for mtb in
+                 sttns[station_id].get_metabhf()]))
+
+            start_end_count_str = ';{};{}'.format(sttns[station_id].get_start_count(),
+                                                  sttns[station_id].get_end_count())
+
+            coords_str = ';{};{}'.format(sttns[station_id].get_lat(), sttns[station_id].get_long())
+
+            if station_id in platforms.keys():
+                platforms_count_str = ';{}'.format(sttns[station_id].get_platforms_count())
+            else:
+                platforms_count_str = ';{}'.format(0)
+
+            mobihub_str = ';{}'.format(sttns[station_id].mobihub_id)
+
+            f.write(
+                station_str +
+                mobihub_str +
+                vehicle_categories_str +
+                metabhf_count_str +
+                metabhf_str +
+                platforms_count_str +
+                start_end_count_str +
+                coords_str + '\n')
